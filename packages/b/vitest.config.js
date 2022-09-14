a9853946ee109ec532dbf400912c69e43093df6b
Alex LaFroscia <a_lafroscia@apple.com>
2022-09-14 12:45:03 -0400

    remove "fix" in Vitest config

--- packages/b/vitest.config.js
+++ packages/b/vitest.config.js
@@ -3,7 +3,12 @@ import { defineConfig, configDefaults } from "vitest/config";
 export default defineConfig({
   test: {
     coverage: {
-      exclude: [...configDefaults.coverage.exclude, "packages/a"],
+      exclude: [
+        ...configDefaults.coverage.exclude,
+        // Un-commenting the next line fixes the problem, but is not an intuitive fix
+        // since the path is relative to the workspace root, rather than this file
+        // "packages/a"
+      ],
     },
   },
 });
